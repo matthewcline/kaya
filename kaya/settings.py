a0dd8705e6dc1ad66cf6aedf46111800fcd6852c
Matthew Cline <clinemattw@gmail.com>
2023-04-12 21:20:18 -0400

    rm

--- kaya/settings.py
+++ kaya/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9)e%oo$zo(u&cw(38!!ay#)&@)o$jdeht173u^#)qeatzm^dug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
